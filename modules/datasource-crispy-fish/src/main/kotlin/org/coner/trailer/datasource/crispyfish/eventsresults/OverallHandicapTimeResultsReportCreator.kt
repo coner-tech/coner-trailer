961c241cb2fb46742d4b032b111a62fe2e253366
Carlton Whitehead <cebesius86@gmail.com>
2020-07-05 12:53:48 -0400

    Initial mapping of crispy fish registration-based overall handicap results

---
+++ modules/datasource-crispy-fish/src/main/kotlin/org/coner/trailer/datasource/crispyfish/eventsresults/OverallHandicapTimeResultsReportCreator.kt
@@ -0,0 +1,28 @@
+package org.coner.trailer.datasource.crispyfish.eventsresults
+
+import org.coner.crispyfish.model.Registration
+import org.coner.trailer.Person
+import org.coner.trailer.eventresults.OverallResultsReport
+import org.coner.trailer.eventresults.StandardResultsTypes
+
+class OverallHandicapTimeResultsReportCreator {
+
+    fun createFromRegistrationData(
+            crispyFishRegistrations: List<Registration>,
+            peopleByMemberId: Map<String, Person>
+    ) : OverallResultsReport {
+        val results = crispyFishRegistrations
+                .mapNotNull {
+                    ParticipantResultMapper.map(
+                            crispyFishRegistration = it,
+                            crispyFishResult = it.paxResult,
+                            peopleByMemberId = peopleByMemberId
+                    )
+                }
+                .sortedBy { it.position }
+        return OverallResultsReport(
+                type = StandardResultsTypes.overallHandicapTime,
+                participantResults = results
+        )
+    }
+}
