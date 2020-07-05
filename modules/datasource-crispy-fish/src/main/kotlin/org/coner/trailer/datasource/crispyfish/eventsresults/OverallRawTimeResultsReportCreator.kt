6f9983460e566619ecca84377da5cad4049c1051
Carlton Whitehead <cebesius86@gmail.com>
2020-07-05 12:52:07 -0400

    Initial mapping of crispy fish registration-based overall raw results

---
+++ modules/datasource-crispy-fish/src/main/kotlin/org/coner/trailer/datasource/crispyfish/eventsresults/OverallRawTimeResultsReportCreator.kt
@@ -0,0 +1,28 @@
+package org.coner.trailer.datasource.crispyfish.eventsresults
+
+import org.coner.crispyfish.model.Registration
+import org.coner.trailer.Person
+import org.coner.trailer.eventresults.OverallResultsReport
+import org.coner.trailer.eventresults.StandardResultsTypes
+
+class OverallRawTimeResultsReportCreator {
+
+    fun createFromRegistrationData(
+            crispyFishRegistrations: List<Registration>,
+            peopleByMemberId: Map<String, Person>
+    ) : OverallResultsReport {
+        val results = crispyFishRegistrations
+                .mapNotNull {
+                    ParticipantResultMapper.map(
+                            crispyFishRegistration = it,
+                            crispyFishResult = it.rawResult,
+                            peopleByMemberId = peopleByMemberId
+                    )
+                }
+                .sortedBy { it.position }
+        return OverallResultsReport(
+                type = StandardResultsTypes.overallRawTime,
+                participantResults = results
+        )
+    }
+}
