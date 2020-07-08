9af2406620d03c2b68eb139134295e2e9d3786ec
Carlton Whitehead <cebesius86@gmail.com>
2020-07-07 22:11:19 -0400

    SNAPSHOT Fix typo in ParticipantResultMapper

--- modules/datasource-crispy-fish/src/main/kotlin/org/coner/trailer/datasource/crispyfish/eventsresults/ParticipantResultMapper.kt
+++ modules/datasource-crispy-fish/src/main/kotlin/org/coner/trailer/datasource/crispyfish/eventsresults/ParticipantResultMapper.kt
@@ -1,14 +1,10 @@
 package org.coner.trailer.datasource.crispyfish.eventsresults
 
-import org.coner.crispyfish.filetype.ecf.EventControlFile
 import org.coner.crispyfish.model.Registration
 import org.coner.crispyfish.model.RegistrationResult
 import org.coner.trailer.Person
-import org.coner.trailer.eventresults.Score
-import org.coner.trailer.Time
 import org.coner.trailer.datasource.crispyfish.ParticipantMapper
 import org.coner.trailer.eventresults.ParticipantResult
-import java.math.BigDecimal
 
 object ParticipantResultMapper {
 
@@ -17,13 +13,13 @@ object ParticipantResultMapper {
             cfResult: RegistrationResult,
             peopleByMemberId: Map<String, Person>
     ): ParticipantResult? {
-        val classResultPosition = cfResult.position ?: return null
+        val cfResultPosition = cfResult.position ?: return null
         val scoredRuns = ResultRunMapper.map(
                 crispyFishRegistrationRuns = cfRegistration.runs,
                 crispyFishRegistrationBestRun = cfRegistration.bestRun
         )
         return ParticipantResult(
-                position = classResultPosition,
+                position = cfResultPosition,
                 score = ScoreMapper.map(
                         cfRegistration = cfRegistration,
                         cfResult = cfResult,
