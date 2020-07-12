f7d88ead1127d5af582b43aa7f6355fa68142b28
Carlton Whitehead <cebesius86@gmail.com>
2020-07-12 11:58:38 -0400

    Initial test of season points standings produced by crispy fish data source

--- modules/datasource-crispy-fish/src/test/kotlin/org/coner/trailer/datasource/crispyfish/seasonpoints/SeasonPointsStandingsTest.kt
+++ modules/datasource-crispy-fish/src/test/kotlin/org/coner/trailer/datasource/crispyfish/seasonpoints/SeasonPointsStandingsTest.kt
@@ -2,11 +2,16 @@ package org.coner.trailer.datasource.crispyfish.seasonpoints
 
 import assertk.all
 import assertk.assertThat
+import assertk.assertions.corresponds
+import assertk.assertions.hasSize
+import assertk.assertions.index
+import assertk.assertions.key
+import org.coner.trailer.TestGroupings
+import org.coner.trailer.TestPeople
 import org.coner.trailer.datasource.crispyfish.eventsresults.CompetitionGroupedResultsReportCreator
 import org.coner.trailer.datasource.crispyfish.fixture.SeasonFixture
 import org.coner.trailer.eventresults.StandardResultsTypes
-import org.coner.trailer.seasonpoints.StandingsReportCreator
-import org.coner.trailer.seasonpoints.TestRankingSorts
+import org.coner.trailer.seasonpoints.*
 import org.junit.jupiter.api.Test
 
 class SeasonPointsStandingsTest {
@@ -32,6 +37,36 @@ class SeasonPointsStandingsTest {
 
         val actual = creator.createGroupedStandingsSections(param)
 
-        assertThat(actual).all {  }
+        assertThat(actual).all {
+            key(TestGroupings.Lscc2019.NOV).all {
+                standings().all {
+                    hasSize(4)
+                    index(0).all {
+                        hasPosition(1)
+                        isNotTie()
+                        hasScore(18)
+                        person().corresponds(TestPeople.BRANDY_HUFF) { actual, expected -> actual.id == expected.id }
+                    }
+                    index(1).all {
+                        hasPosition(2)
+                        isNotTie()
+                        hasScore(12)
+                        person().corresponds(TestPeople.BRYANT_MORAN) { actual, expected -> actual.id == expected.id }
+                    }
+                    index(2).all {
+                        hasPosition(3)
+                        isNotTie()
+                        hasScore(10)
+                        person().corresponds(TestPeople.DOMINIC_ROGERS) { actual, expected -> actual.id == expected.id }
+                    }
+                    index(3).all {
+                        hasPosition(4)
+                        isNotTie()
+                        hasScore(7)
+                        person().corresponds(TestPeople.BENNETT_PANTONE) { actual, expected -> actual.id == expected.id }
+                    }
+                }
+            }
+        }
     }
 }
