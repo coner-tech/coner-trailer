e7f9801fd57030dc928041ded7439d449e69d3b5
Carlton Whitehead <cebesius86@gmail.com>
2020-07-19 13:43:48 -0400

    Unit test SeasonEvent comparison behaviors

--- modules/core-test/src/test/kotlin/org/coner/trailer/SeasonEventTest.kt
+++ modules/core-test/src/test/kotlin/org/coner/trailer/SeasonEventTest.kt
@@ -1,8 +1,13 @@
 package org.coner.trailer
 
+import assertk.assertThat
+import assertk.assertions.isNegative
+import assertk.assertions.isPositive
+import assertk.assertions.isZero
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import java.time.LocalDate
 
 class SeasonEventTest {
 
@@ -35,7 +40,62 @@ class SeasonEventTest {
     }
 
     @Test
-    fun `It should compare events correctly`() {
-        TODO()
+    fun `It should compare SeasonEvents with eventNumbers`() {
+        val one = TestSeasonEvents.Lscc2019Simplified.points1
+        val two = TestSeasonEvents.Lscc2019Simplified.points2
+
+        // one to two
+        val actualOneToTwo = one.compareTo(two)
+
+        assertThat(actualOneToTwo, "comparing one to two").isNegative()
+
+        // two to one
+        val actualTwoToOne = two.compareTo(one)
+
+        assertThat(actualTwoToOne, "comparing two to one").isPositive()
+
+        // one to one
+        val actualOneToOne = one.compareTo(one)
+
+        assertThat(actualOneToOne, "comparing one to one").isZero()
+    }
+
+    @Test
+    fun `It should compare SeasonEvents without eventNumbers`() {
+        val one = TestSeasonEvents.Lscc2019Simplified.points1
+        val testAndTune = SeasonEvent(
+                event = Event(
+                        date = LocalDate.parse("2019-02-01"), // important
+                        name = "Test and Tune"
+                ),
+                points = false,
+                eventNumber = null
+        )
+        val two = TestSeasonEvents.Lscc2019Simplified.points2
+
+        // one to test and tune
+        val actualOneToTestAndTune = one.compareTo(testAndTune)
+
+        assertThat(actualOneToTestAndTune, "comparing one to test and tune").isNegative()
+
+        // test and tune to one
+        val actualTestAndTuneToOne = testAndTune.compareTo(one)
+
+        assertThat(actualTestAndTuneToOne, "comparing test and tune to one").isPositive()
+
+        // test and tune to self
+        val actualSelfToSelf = testAndTune.compareTo(testAndTune)
+
+        assertThat(actualSelfToSelf, "comparing test and tune to self").isZero()
+
+        // test and tune to two
+        val actualTestAndTuneToTwo = testAndTune.compareTo(two)
+
+        assertThat(actualTestAndTuneToTwo, "comparing test and tune to two").isNegative()
+
+        // two to test and tune
+        val actualTwoToTestAndTune = two.compareTo(testAndTune)
+
+        assertThat(actualTwoToTestAndTune, "comparing two to test and tune").isPositive()
     }
 }
