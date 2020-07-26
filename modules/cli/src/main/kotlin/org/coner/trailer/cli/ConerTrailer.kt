32310a04ecbdfaaaf7741979a520942ca2bef3e1
Carlton Whitehead <cebesius86@gmail.com>
2020-07-26 13:46:54 -0400

    Clean up cli app main indendation

--- modules/cli/src/main/kotlin/org/coner/trailer/cli/ConerTrailer.kt
+++ modules/cli/src/main/kotlin/org/coner/trailer/cli/ConerTrailer.kt
@@ -1,16 +1,16 @@
 package org.coner.trailer.cli
 
-import com.github.ajalt.clikt.core.*
+import com.github.ajalt.clikt.core.subcommands
 import org.coner.trailer.cli.command.ConerTrailer
 import org.coner.trailer.cli.command.Config
 
-fun main(args: Array<out String>) = ConerTrailer()
-        .subcommands(
-                Config()
-                        .subcommands(
-                                Config.ListDatabases(),
-                                Config.RemoveDatabase(),
-                                Config.Database()
-                        )
-        )
-        .main(args)
+fun main(args: Array<out String>) {
+    val app = ConerTrailer().subcommands(
+            Config().subcommands(
+                    Config.ListDatabases(),
+                    Config.RemoveDatabase(),
+                    Config.Database()
+            )
+    )
+    app.main(args)
+}
