1c9c416912a9f350c5b4afe099b85d775cc4c824
Carlton Whitehead <cebesius86@gmail.com>
2020-09-09 18:57:02 -0400

    SNAPSHOT season points calculators

---
+++ modules/cli/src/main/kotlin/org/coner/trailer/cli/command/seasonpointscalculator/SeasonPointsCalculatorSetCommand.kt
@@ -0,0 +1,57 @@
+package org.coner.trailer.cli.command.seasonpointscalculator
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.core.findOrSetObject
+import com.github.ajalt.clikt.output.CliktConsole
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.convert
+import com.github.ajalt.clikt.parameters.options.*
+import org.coner.trailer.cli.util.clikt.toUuid
+import org.coner.trailer.cli.view.SeasonPointsCalculatorConfigurationView
+import org.coner.trailer.io.service.RankingSortService
+import org.coner.trailer.io.service.SeasonPointsCalculatorConfigurationService
+import org.coner.trailer.seasonpoints.RankingSort
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.instance
+import java.util.*
+
+class SeasonPointsCalculatorSetCommand(
+        di: DI,
+        useConsole: CliktConsole
+) : CliktCommand(
+        name = "set",
+        help = "Set a season points calculator"
+), DIAware {
+
+    init {
+        context {
+            console = useConsole
+        }
+    }
+
+    override val di: DI by findOrSetObject { di }
+
+    private val rankingSortService: RankingSortService by instance()
+    private val service: SeasonPointsCalculatorConfigurationService by instance()
+    private val view: SeasonPointsCalculatorConfigurationView by instance()
+
+    private val id: UUID by argument()
+            .convert { toUuid(it) }
+
+    private val name: String? by option()
+    private val resultsTypeKeyToParticipantEventResultPointsCalculatorNamed: List<Pair<String, String>> by option(
+            metavar = "KEY NAME..."
+    )
+            .pair()
+            .multiple()
+    private val rankingSortNamed: RankingSort? by option(metavar = "NAME")
+            .convert {
+                rankingSortService.findByName(it)
+                        ?: fail("No ranking sort found with name: $it")
+            }
+
+    override fun run() {
+    }
+}
