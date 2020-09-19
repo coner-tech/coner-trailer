c8e155c1f28b4486f969396371e9c36b55d58951
Carlton Whitehead <cebesius86@gmail.com>
2020-09-18 20:30:04 -0400

    Move maven modules into root folder

---
+++ cli/src/main/kotlin/org/coner/trailer/cli/command/person/PersonSetCommand.kt
@@ -0,0 +1,70 @@
+package org.coner.trailer.cli.command.person
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.core.findOrSetObject
+import com.github.ajalt.clikt.output.CliktConsole
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.convert
+import com.github.ajalt.clikt.parameters.options.*
+import org.coner.trailer.Person
+import org.coner.trailer.cli.util.clikt.toUuid
+import org.coner.trailer.cli.view.PersonView
+import org.coner.trailer.io.service.PersonService
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.instance
+import java.util.*
+
+class PersonSetCommand(
+        di: DI,
+        useConsole: CliktConsole
+) : CliktCommand(
+        name = "set",
+        help = "Set a person"
+), DIAware {
+
+    init {
+        context {
+            console = useConsole
+        }
+    }
+
+    override val di: DI by findOrSetObject { di }
+
+    private val service: PersonService by instance()
+    private val view: PersonView by instance()
+
+    private val id: UUID by argument()
+            .convert { toUuid(it) }
+    sealed class MemberIdOption {
+        data class Set(val value: String) : MemberIdOption()
+        object Unset : MemberIdOption()
+    }
+    private val memberId: MemberIdOption? by option(
+            "--member-id",
+            help = "Set the member ID to VALUE. Pass \"null\" to unset."
+    )
+            .convert { when {
+                it.equals("null", ignoreCase = true) -> MemberIdOption.Unset
+                else -> MemberIdOption.Set(it)
+            } }
+    private val firstName: String? by option()
+    private val lastName: String? by option()
+
+    override fun run() {
+        val current = service.findById(id)
+        val set = Person(
+                id = current.id,
+                memberId = when (val memberId = memberId) {
+                    is MemberIdOption.Set -> memberId.value
+                    is MemberIdOption.Unset -> null
+                    null -> current.memberId
+                },
+                firstName = firstName ?: current.firstName,
+                lastName = lastName ?: current.lastName
+        )
+        service.update(set)
+        echo(view.render(set))
+    }
+}
---
+++ core-test/src/main/kotlin/org/coner/trailer/GroupingAssertk.kt
@@ -0,0 +1,22 @@
+package org.coner.trailer
+
+import assertk.Assert
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import assertk.assertions.prop
+
+fun Assert<Grouping>.name() = prop("name") { it.name }
+fun Assert<Grouping>.hasName(expected: String) = name().isEqualTo(expected)
+
+fun Assert<Grouping>.abbreviation() = prop("abbreviation") { it.abbreviation }
+fun Assert<Grouping>.hasAbbreviation(expected: String) = abbreviation().isEqualTo(expected)
+
+fun Assert<Grouping>.sort() = prop("sort") { it.sort }
+fun Assert<Grouping>.hasSort(expected: Int) = sort().isEqualTo(expected)
+
+fun Assert<Grouping>.isSingular() = isInstanceOf(Grouping.Singular::class)
+
+fun Assert<Grouping>.isPaired() = isInstanceOf(Grouping.Paired::class)
+fun Assert<Grouping.Paired>.pair() = prop("pair") { it.pair }
+fun Assert<Grouping.Paired>.first() = pair().prop("first") { it.first }
+fun Assert<Grouping.Paired>.second() = pair().prop("second") { it.second}
