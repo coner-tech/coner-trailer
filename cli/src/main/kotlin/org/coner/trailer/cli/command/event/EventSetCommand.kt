ca069b958a50f943b83ff711a4ed49fd95a23e40
Carlton Whitehead <cebesius86@gmail.com>
2020-12-07 18:56:10 -0500

    SNAPSHOT set event

--- cli/src/main/kotlin/org/coner/trailer/cli/command/event/EventSetCommand.kt
+++ cli/src/main/kotlin/org/coner/trailer/cli/command/event/EventSetCommand.kt
@@ -5,11 +5,12 @@ import com.github.ajalt.clikt.core.findOrSetObject
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.convert
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
-import com.github.ajalt.clikt.parameters.groups.groupSwitch
+import com.github.ajalt.clikt.parameters.groups.groupChoice
 import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.path
 import org.coner.trailer.Event
+import org.coner.trailer.cli.io.DatabaseConfiguration
 import org.coner.trailer.cli.util.clikt.toLocalDate
 import org.coner.trailer.cli.util.clikt.toUuid
 import org.coner.trailer.cli.view.EventView
@@ -30,6 +31,7 @@ class EventSetCommand(
 
     override val di: DI by findOrSetObject { di }
 
+    private val dbConfig: DatabaseConfiguration by instance()
     private val service: EventService by instance()
     private val view: EventView by instance()
 
@@ -37,9 +39,9 @@ class EventSetCommand(
     private val name: String? by option()
     private val date: LocalDate? by option().convert { toLocalDate(it) }
     private val crispyFish: CrispyFishOptions? by option()
-        .groupSwitch(
-            "--set-crispy-fish" to CrispyFishOptions.Set(),
-            "--unset-crispy-fish" to CrispyFishOptions.Unset
+        .groupChoice(
+            "set" to CrispyFishOptions.Set(),
+            "unset" to CrispyFishOptions.Unset
         )
     sealed class CrispyFishOptions : OptionGroup() {
         class Set : CrispyFishOptions() {
@@ -69,11 +71,14 @@ class EventSetCommand(
             crispyFish = when (val crispyFishOptions = crispyFish) {
                 is CrispyFishOptions.Set -> {
                     Event.CrispyFishMetadata(
-                        eventControlFile = crispyFishOptions.eventControlFile?.toString()
-                            ?: requireNotNull(event.crispyFish?.eventControlFile) { "Missing --event-control-file" },
-                        classDefinitionFile = crispyFishOptions.classDefinitionFile?.toString()
-                            ?: requireNotNull(event.crispyFish?.classDefinitionFile) { "Missing --class-definition-file"  },
-                        forceParticipantSignageToPerson = requireNotNull(event.crispyFish?.forceParticipantSignageToPerson)
+                        eventControlFile = crispyFishOptions.eventControlFile?.let {
+                            dbConfig.crispyFishDatabase.relativize(it).toString()
+                        } ?: requireNotNull(event.crispyFish?.eventControlFile) { "Missing --event-control-file" },
+                        classDefinitionFile = crispyFishOptions.classDefinitionFile?.let {
+                            dbConfig.crispyFishDatabase.relativize(it).toString()
+                        } ?: requireNotNull(event.crispyFish?.classDefinitionFile) { "Missing --class-definition-file" },
+                        forceParticipantSignageToPerson = event.crispyFish?.forceParticipantSignageToPerson
+                            ?: emptyMap()
                     )
                 }
                 is CrispyFishOptions.Unset -> null
--- cli/src/test/kotlin/org/coner/trailer/cli/command/event/EventSetCommandTest.kt
+++ cli/src/test/kotlin/org/coner/trailer/cli/command/event/EventSetCommandTest.kt
@@ -1,33 +1,54 @@
 package org.coner.trailer.cli.command.event
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import com.github.ajalt.clikt.core.context
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.justRun
+import io.mockk.verifySequence
+import org.coner.trailer.Event
+import org.coner.trailer.TestEvents
 import org.coner.trailer.cli.clikt.StringBufferConsole
+import org.coner.trailer.cli.io.DatabaseConfiguration
 import org.coner.trailer.cli.view.EventView
 import org.coner.trailer.io.service.EventService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.io.TempDir
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.instance
+import java.nio.file.Path
+import java.time.LocalDate
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.createDirectory
+import kotlin.io.path.createFile
 
+@ExperimentalPathApi
 @ExtendWith(MockKExtension::class)
 class EventSetCommandTest {
 
     lateinit var command: EventSetCommand
 
+    @MockK lateinit var dbConfig: DatabaseConfiguration
     @MockK lateinit var service: EventService
     @MockK lateinit var view: EventView
 
     lateinit var testConsole: StringBufferConsole
 
+    @TempDir lateinit var root: Path
+    lateinit var crispyFish: Path
+
     @BeforeEach
     fun before() {
         testConsole = StringBufferConsole()
+        crispyFish = root.resolve("crispy-fish").createDirectory()
         command = EventSetCommand(
             di = DI {
+                bind<DatabaseConfiguration>() with instance(dbConfig)
                 bind<EventService>() with instance(service)
                 bind<EventView>() with instance(view)
             }
@@ -40,7 +61,39 @@ class EventSetCommandTest {
 
     @Test
     fun `It should set event properties`() {
-        TODO()
+        val original = TestEvents.Lscc2019.points1
+        val set = original.copy(
+            name = "It should set event properties",
+            date = LocalDate.parse("2020-12-07"),
+            crispyFish = Event.CrispyFishMetadata(
+                eventControlFile = "set-event-control-file.ecf",
+                classDefinitionFile = "set-class-definition-file.ecf",
+                forceParticipantSignageToPerson = emptyMap()
+            )
+        )
+        val setEventControlFile = crispyFish.resolve(set.crispyFish!!.eventControlFile).createFile()
+        val setClassDefinitionFile = crispyFish.resolve(set.crispyFish!!.classDefinitionFile).createFile()
+        every { dbConfig.crispyFishDatabase } returns crispyFish
+        every { service.findById(set.id) } returns original
+        justRun { service.update(eq(set)) }
+        val viewRendered = "view rendered set event named: ${set.name}"
+        every { view.render(set) } returns viewRendered
+
+        command.parse(arrayOf(
+            "${original.id}",
+            "--name", set.name,
+            "--date", "${set.date}",
+            "--crispy-fish", "set",
+            "--event-control-file", "$setEventControlFile",
+            "--class-definition-file", "$setClassDefinitionFile"
+        ))
+
+        verifySequence {
+            service.findById(original.id)
+            service.update(eq(set))
+            view.render(eq(set))
+        }
+        assertThat(testConsole.output).isEqualTo(viewRendered)
     }
 
     @Test
