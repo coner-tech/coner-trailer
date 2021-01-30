1a14c002d95dfa73cd45ef7c645756dba1fec67f
Carlton Whitehead <cebesius86@gmail.com>
2021-01-30 08:29:38 -0500

    SNAPSHOT EventCheckCommandTest

--- cli/src/test/kotlin/org/coner/trailer/cli/command/event/EventCheckCommandTest.kt
+++ cli/src/test/kotlin/org/coner/trailer/cli/command/event/EventCheckCommandTest.kt
@@ -1,11 +1,19 @@
 package org.coner.trailer.cli.command.event
 
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.contains
 import com.github.ajalt.clikt.core.context
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verifySequence
+import org.coner.trailer.Event
 import org.coner.trailer.cli.clikt.StringBufferConsole
 import org.coner.trailer.cli.view.CrispyFishRegistrationTableView
 import org.coner.trailer.cli.view.PeopleMapKeyTableView
+import org.coner.trailer.datasource.crispyfish.CrispyFishEventMappingContext
 import org.coner.trailer.io.service.CrispyFishEventMappingContextService
 import org.coner.trailer.io.service.EventService
 import org.junit.jupiter.api.BeforeEach
@@ -14,6 +22,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.instance
+import java.util.*
 import kotlin.io.path.ExperimentalPathApi
 
 @ExperimentalPathApi
@@ -44,11 +53,57 @@ class EventCheckCommandTest {
 
     @Test
     fun `It should check event and report all problems`() {
-        TODO()
+        val checkId = UUID.randomUUID()
+        val checkCrispyFish: Event.CrispyFishMetadata = mockk()
+        val check: Event = mockk {
+            every { id } returns checkId
+            every { crispyFish } returns checkCrispyFish
+        }
+        val context: CrispyFishEventMappingContext = mockk()
+        val result = EventService.CheckResult(
+            unmappedClubMemberIdNullRegistrations = listOf(mockk()),
+            unmappedClubMemberIdNotFoundRegistrations = listOf(mockk()),
+            unmappedClubMemberIdAmbiguousRegistrations = listOf(mockk()),
+            unmappedClubMemberIdMatchButNameMismatchRegistrations = listOf(mockk()),
+            unmappedExactMatchRegistrations = listOf(mockk()),
+            unusedPeopleMapKeys = listOf(mockk())
+        )
+        every { service.findById(checkId) } returns check
+        every { crispyFishEventMappingContextService.load(checkCrispyFish) } returns context
+        every { service.check(check, context) } returns result
+        every { registrationTableView.render(any()) } returns "registrationTableView rendered"
+        every { peopleMapKeyTableView.render(any()) } returns "peopleMapKeyTableView rendered"
+
+        command.parse(arrayOf("$checkId"))
+
+        verifySequence {
+            service.findById(checkId)
+            crispyFishEventMappingContextService.load(checkCrispyFish)
+            service.check(check, context)
+            registrationTableView.render(result.unmappedClubMemberIdNullRegistrations)
+            registrationTableView.render(result.unmappedClubMemberIdNotFoundRegistrations)
+            registrationTableView.render(result.unmappedClubMemberIdAmbiguousRegistrations)
+            registrationTableView.render(result.unmappedClubMemberIdMatchButNameMismatchRegistrations)
+            registrationTableView.render(result.unmappedExactMatchRegistrations)
+            peopleMapKeyTableView.render(result.unusedPeopleMapKeys)
+        }
+        assertThat(useConsole.output).all {
+            contains("Found unmapped registration(s) with club member ID null")
+            contains("Found unmapped registration(s) with club member ID not found")
+            contains("Found unmapped registration(s) with club member ID ambiguous")
+            contains("Found unmapped registration(s) with club member ID match but name mismatch")
+            contains("Found unmapped registration(s) with exact matching people")
+            contains("Found unused people map keys")
+        }
     }
 
     @Test
     fun `It should check event and finish silently if there are no problems`() {
+        TODO()
+    }
+
+    @Test
+    fun `It should throw when event is missing crispy fish metadata`() {
         TODO()
     }
 }
