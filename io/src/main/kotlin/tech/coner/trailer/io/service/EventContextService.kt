eae05ff33ef248ccb5f399b18afaee8504ae4394
Carlton Whitehead <cebesius86@gmail.com>
2022-07-06 23:43:01 -0400

    SNAPSHOT miscellaneous dev
    Kotlin Coroutines adoption and integration

--- io/src/main/kotlin/tech/coner/trailer/io/service/EventContextService.kt
+++ io/src/main/kotlin/tech/coner/trailer/io/service/EventContextService.kt
@@ -1,23 +1,31 @@
 package tech.coner.trailer.io.service
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import tech.coner.trailer.Event
 import tech.coner.trailer.EventContext
+import tech.coner.trailer.io.util.runSuspendCatching
 import kotlin.coroutines.CoroutineContext
 
 class EventContextService(
-    coroutineContext: CoroutineContext,
+    override val coroutineContext: CoroutineContext,
     private val classService: ClassService,
     private val participantService: ParticipantService,
     private val runService: RunService,
     private val eventExtendedParametersService: EventExtendedParametersService
-) : CoroutineContext by coroutineContext {
+) : CoroutineScope {
 
-    suspend fun load(event: Event): Result<EventContext> = runCatching {
+    suspend fun load(event: Event): Result<EventContext> = runSuspendCatching {
+        val classesAsync = async { classService.list(event).getOrThrow() }
+        val participantsAsync = async { participantService.list(event).getOrThrow() }
+        val runsAsync = async { runService.list(event).getOrThrow() }
+        val extendedParametersAsync = async { eventExtendedParametersService.load(event).getOrThrow() }
         EventContext(
-            classes = classService.list(event).getOrThrow(),
-            participants = participantService.list(event).getOrThrow(),
-            runs = runService.list(event).getOrThrow(),
-            extendedParameters = eventExtendedParametersService.load(event).getOrThrow()
+            classes = classesAsync.await(),
+            participants = participantsAsync.await(),
+            runs = runsAsync.await(),
+            extendedParameters = extendedParametersAsync.await()
         )
     }
+
 }
