ee70dab2bdb94514815b447bf356f2c0358f7c75
Carlton Whitehead <cebesius86@gmail.com>
2022-10-21 07:32:23 -0400

    SNAPSHOT results web scaffold
    TODOs:
    - [x] import ktor server
    - [x] start server with CLI
    - [x] extend kodein DI in webapp
    - [x] implement scaffold hello world resource
    - [x] decouple resource from webapp (not single file)
    - [x] test scaffold hello world resource
    - [x] fix configuration saving and improve safety
    - [ ] kotlinx-html UI for hello world resource
    - [ ] integration test

--- assertk-ktor/src/main/kotlin/tech/coner/trailer/assertk/ktor/KtorAssertk.kt
+++ assertk-ktor/src/main/kotlin/tech/coner/trailer/assertk/ktor/KtorAssertk.kt
@@ -2,8 +2,10 @@ package tech.coner.trailer.assertk.ktor
 
 import assertk.Assert
 import assertk.assertions.prop
-import io.ktor.client.statement.*
-import io.ktor.http.*
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.isSuccess
 import kotlinx.coroutines.runBlocking
 
 fun Assert<HttpResponse>.status() = prop(HttpResponse::status)
