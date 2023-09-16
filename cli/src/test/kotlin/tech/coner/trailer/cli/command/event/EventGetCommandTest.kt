5a1ac5f96f1ead7282b2f2a2c8d7329ab5726e0a
Carlton Whitehead <cebesius86@gmail.com>
2023-09-15 20:27:19 -0400

    Fix EventGetCommandTest

--- cli/src/test/kotlin/tech/coner/trailer/cli/command/event/EventGetCommandTest.kt
+++ cli/src/test/kotlin/tech/coner/trailer/cli/command/event/EventGetCommandTest.kt
@@ -2,8 +2,7 @@ package tech.coner.trailer.cli.command.event
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import io.mockk.coEvery
-import io.mockk.coVerifySequence
+import io.mockk.*
 import org.junit.jupiter.api.Test
 import org.kodein.di.DirectDI
 import org.kodein.di.instance
@@ -11,12 +10,14 @@ import org.kodein.di.on
 import tech.coner.trailer.TestEvents
 import tech.coner.trailer.cli.command.BaseDataSessionCommandTest
 import tech.coner.trailer.io.service.EventService
+import tech.coner.trailer.presentation.adapter.EventDetailModelAdapter
 import tech.coner.trailer.presentation.model.EventDetailModel
 import tech.coner.trailer.presentation.text.view.TextView
 
 class EventGetCommandTest : BaseDataSessionCommandTest<EventGetCommand>() {
 
     private val service: EventService by instance()
+    private val adapter: EventDetailModelAdapter by instance()
     private val view: TextView<EventDetailModel> by instance()
 
     override fun DirectDI.createCommand() = instance<EventGetCommand>()
@@ -25,9 +26,10 @@ class EventGetCommandTest : BaseDataSessionCommandTest<EventGetCommand>() {
     fun `It should get event by ID`() {
         val event = TestEvents.Lscc2019.points1
         coEvery { service.findByKey(event.id) } returns Result.success(event)
+        val model: EventDetailModel = mockk()
+        every { adapter(any()) } returns model
         val viewRendered = "view rendered event ${event.id}"
-
-//        every { view(event) } returns viewRendered
+        every { view(any()) } returns viewRendered
 
         command.parse(arrayOf(
             "${event.id}"
@@ -35,8 +37,10 @@ class EventGetCommandTest : BaseDataSessionCommandTest<EventGetCommand>() {
 
         coVerifySequence {
             service.findByKey(event.id)
-//            view(event)
+            adapter(event)
+            view(model)
         }
+        confirmVerified(service, adapter, view)
         assertThat(testConsole.output).isEqualTo(viewRendered)
     }
 }
