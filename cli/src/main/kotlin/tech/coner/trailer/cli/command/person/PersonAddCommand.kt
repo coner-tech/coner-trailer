06917149e6fd55ab570bf77e0de6e813a3293f65
Carlton Whitehead <cebesius86@gmail.com>
2023-09-16 18:29:25 -0400

    Fix PersonAddCommand and test

--- cli/src/main/kotlin/tech/coner/trailer/cli/command/person/PersonAddCommand.kt
+++ cli/src/main/kotlin/tech/coner/trailer/cli/command/person/PersonAddCommand.kt
@@ -54,7 +54,7 @@ class PersonAddCommand(
         presenter.commit()
             .succeedOrThrow {
                 presenter.create()
-                view(it)
+                echo(view(it))
             }
     }
 }
--- cli/src/test/kotlin/tech/coner/trailer/cli/command/person/PersonAddCommandTest.kt
+++ cli/src/test/kotlin/tech/coner/trailer/cli/command/person/PersonAddCommandTest.kt
@@ -2,30 +2,42 @@ package tech.coner.trailer.cli.command.person
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import io.mockk.justRun
-import io.mockk.verifySequence
+import io.mockk.*
 import org.junit.jupiter.api.Test
 import org.kodein.di.DirectDI
+import org.kodein.di.factory
 import org.kodein.di.instance
 import tech.coner.trailer.TestPeople
 import tech.coner.trailer.cli.command.BaseDataSessionCommandTest
-import tech.coner.trailer.io.service.PersonService
 import tech.coner.trailer.presentation.model.PersonDetailModel
+import tech.coner.trailer.presentation.presenter.person.PersonDetailPresenter
+import tech.coner.trailer.presentation.presenter.person.PersonDetailPresenterFactory
 import tech.coner.trailer.presentation.text.view.TextView
 
 class PersonAddCommandTest : BaseDataSessionCommandTest<PersonAddCommand>() {
 
-    private val service: PersonService by instance()
+    private val presenterFactory: PersonDetailPresenterFactory by factory()
     private val view: TextView<PersonDetailModel> by instance()
 
     override fun DirectDI.createCommand() = instance<PersonAddCommand>()
 
     @Test
     fun `It should add person`() {
+        val presenter = presenterFactory(PersonDetailPresenter.Argument.Create)
+        val initialModel: PersonDetailModel = mockk {
+            justRun { setId(any()) }
+            justRun { setClubMemberId(any()) }
+            justRun { setFirstName(any()) }
+            justRun { setLastName(any()) }
+            justRun { setMotorsportRegId(any()) }
+        }
+        val committedModel: PersonDetailModel = mockk()
+        every { presenter.itemModel } returns initialModel
+        every { presenter.commit() } returns Result.success(committedModel)
+        justRun { presenter.create() }
         val person = TestPeople.ANASTASIA_RIGLER
-        justRun { service.create(eq(person)) }
         val viewRendered = "view rendered ${person.lastName} ${person.lastName}"
-//        every { view.render(eq(person)) } returns viewRendered
+        every { view(any()) } returns viewRendered
 
         command.parse(arrayOf(
                 "--id", person.id.toString(),
@@ -35,10 +47,18 @@ class PersonAddCommandTest : BaseDataSessionCommandTest<PersonAddCommand>() {
                 "--motorsportreg-member-id", "${person.motorsportReg?.memberId}"
         ))
 
-        verifySequence {
-            service.create(eq(person))
-//            view.render(eq(person))
-        }
         assertThat(testConsole.output, "console output").isEqualTo(viewRendered)
+        verifySequence {
+            presenter.itemModel
+            initialModel.setId(person.id)
+            initialModel.setClubMemberId(person.clubMemberId!!)
+            initialModel.setFirstName(person.firstName)
+            initialModel.setLastName(person.lastName)
+            initialModel.setMotorsportRegId(person.motorsportReg?.memberId!!)
+            presenter.commit()
+            presenter.create()
+            view(committedModel)
+        }
+        confirmVerified(presenter, view, initialModel, committedModel)
     }
 }
