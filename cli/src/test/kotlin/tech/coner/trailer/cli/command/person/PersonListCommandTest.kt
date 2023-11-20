88b7012606700762b0c56e5a6ae1facaf9632d7a
Carlton Whitehead <cebesius86@gmail.com>
2023-11-20 11:46:51 -0500

    Fix PersonListCommandTest

--- cli/src/test/kotlin/tech/coner/trailer/cli/command/person/PersonListCommandTest.kt
+++ cli/src/test/kotlin/tech/coner/trailer/cli/command/person/PersonListCommandTest.kt
@@ -3,13 +3,16 @@ package tech.coner.trailer.cli.command.person
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.mockk.every
+import io.mockk.mockk
 import io.mockk.verifySequence
 import org.junit.jupiter.api.Test
 import org.kodein.di.DirectDI
 import org.kodein.di.instance
+import tech.coner.trailer.Person
 import tech.coner.trailer.TestPeople
 import tech.coner.trailer.cli.command.BaseDataSessionCommandTest
 import tech.coner.trailer.io.service.PersonService
+import tech.coner.trailer.presentation.adapter.Adapter
 import tech.coner.trailer.presentation.model.PersonCollectionModel
 import tech.coner.trailer.presentation.model.PersonDetailModel
 import tech.coner.trailer.presentation.text.view.TextCollectionView
@@ -17,6 +20,7 @@ import tech.coner.trailer.presentation.text.view.TextCollectionView
 class PersonListCommandTest : BaseDataSessionCommandTest<PersonListCommand>() {
 
     private val service: PersonService by instance()
+    private val adapter: Adapter<Collection<Person>, PersonCollectionModel> by instance()
     private val view: TextCollectionView<PersonDetailModel, PersonCollectionModel> by instance()
 
     override fun DirectDI.createCommand() = instance<PersonListCommand>()
@@ -30,13 +34,16 @@ class PersonListCommandTest : BaseDataSessionCommandTest<PersonListCommand>() {
         )
         every { service.list() } returns people
         val viewRendered = "view rendered ${people.size} people"
-//        every { view.render(people) } returns viewRendered
+        val model: PersonCollectionModel = mockk()
+        every { adapter(any()) } returns model
+        every { view(any()) } returns viewRendered
 
         command.parse(arrayOf())
 
         verifySequence {
             service.list()
-//            view.render(people)
+            adapter(people)
+            view(model)
         }
         assertThat(testConsole.output, "console output").isEqualTo(viewRendered)
     }
