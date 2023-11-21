794ce51843afacc0f8eb460a2d64fc1dae44c9af
Carlton Whitehead <cebesius86@gmail.com>
2023-11-21 07:42:40 -0500

    Fix MotorsportRegMemberImportCommandTest

--- cli/src/test/kotlin/tech/coner/trailer/cli/command/motorsportreg/MotorsportRegMemberImportCommandTest.kt
+++ cli/src/test/kotlin/tech/coner/trailer/cli/command/motorsportreg/MotorsportRegMemberImportCommandTest.kt
@@ -8,8 +8,10 @@ import io.mockk.verifySequence
 import org.junit.jupiter.api.Test
 import org.kodein.di.DirectDI
 import org.kodein.di.instance
+import tech.coner.trailer.Person
 import tech.coner.trailer.cli.command.BaseDataSessionCommandTest
 import tech.coner.trailer.io.service.MotorsportRegImportService
+import tech.coner.trailer.presentation.adapter.Adapter
 import tech.coner.trailer.presentation.model.PersonCollectionModel
 import tech.coner.trailer.presentation.model.PersonDetailModel
 import tech.coner.trailer.presentation.text.view.TextCollectionView
@@ -17,6 +19,7 @@ import tech.coner.trailer.presentation.text.view.TextCollectionView
 class MotorsportRegMemberImportCommandTest : BaseDataSessionCommandTest<MotorsportRegMemberImportCommand>() {
 
     private val service: MotorsportRegImportService by instance()
+    private val adapter: Adapter<Collection<Person>, PersonCollectionModel> by instance()
     private val view: TextCollectionView<PersonDetailModel, PersonCollectionModel> by instance()
 
     override fun DirectDI.createCommand() = instance<MotorsportRegMemberImportCommand>()
@@ -24,27 +27,33 @@ class MotorsportRegMemberImportCommandTest : BaseDataSessionCommandTest<Motorspo
     @Test
     fun `It should actually import members as people`() {
         val result = MotorsportRegImportService.ImportMembersAsPeopleResult(
-                updated = listOf(mockk()),
+                updated = listOf(mockk(), mockk()),
                 created = listOf(mockk())
         )
         every { service.importMembersAsPeople(false) } returns result
+        val createdModel: PersonCollectionModel = mockk()
+        every { adapter(result.created) } returns createdModel
         val viewRenderedCreated = "view rendered created"
-//        every { view.render(result.created) } returns viewRenderedCreated
+        every { view(createdModel) } returns viewRenderedCreated
+        val updatedModel: PersonCollectionModel = mockk()
+        every { adapter(result.updated) } returns updatedModel
         val viewRenderedUpdated = "view rendered updated"
-//        every { view.render(result.updated) } returns viewRenderedUpdated
+        every { view(updatedModel) } returns viewRenderedUpdated
 
         command.parse(arrayOf())
 
         verifySequence {
             service.importMembersAsPeople(false)
-//            view.render(result.created)
-//            view.render(result.updated)
+            adapter(result.created)
+            view(createdModel)
+            adapter(result.updated)
+            view(updatedModel)
         }
         assertThat(testConsole.output).isEqualTo("""
             Created: (1)
             $viewRenderedCreated
             
-            Updated: (1)
+            Updated: (2)
             $viewRenderedUpdated
         """.trimIndent())
     }
@@ -52,14 +61,18 @@ class MotorsportRegMemberImportCommandTest : BaseDataSessionCommandTest<Motorspo
     @Test
     fun `It should dry-run import members as people`() {
         val result = MotorsportRegImportService.ImportMembersAsPeopleResult(
-                updated = listOf(mockk()),
+                updated = listOf(mockk(), mockk()),
                 created = listOf(mockk())
         )
         every { service.importMembersAsPeople(true) } returns result
+        val createdModel: PersonCollectionModel = mockk()
+        every { adapter(result.created) } returns createdModel
         val viewRenderedCreated = "view rendered created"
-//        every { view.render(result.created) } returns viewRenderedCreated
+        every { view(createdModel) } returns viewRenderedCreated
+        val updatedModel: PersonCollectionModel = mockk()
+        every { adapter(result.updated) } returns updatedModel
         val viewRenderedUpdated = "view rendered updated"
-//        every { view.render(result.updated) } returns viewRenderedUpdated
+        every { view(updatedModel) } returns viewRenderedUpdated
 
         command.parse(arrayOf(
                 "--dry-run"
@@ -67,14 +80,16 @@ class MotorsportRegMemberImportCommandTest : BaseDataSessionCommandTest<Motorspo
 
         verifySequence {
             service.importMembersAsPeople(true)
-//            view.render(result.created)
-//            view.render(result.updated)
+            adapter(result.created)
+            view(createdModel)
+            adapter(result.updated)
+            view(updatedModel)
         }
         assertThat(testConsole.output).isEqualTo("""
             Created: (1)
             $viewRenderedCreated
             
-            Updated: (1)
+            Updated: (2)
             $viewRenderedUpdated
         """.trimIndent())
     }
