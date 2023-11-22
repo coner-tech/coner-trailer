7ac497c86b55a6aff77e1371813920f66d5d0c53
Carlton Whitehead <cebesius86@gmail.com>
2023-11-22 13:38:04 -0500

    Remove unused presentation-json
    Common model classes have rendered them redundant

--- presentation/presentation-json/src/main/kotlin/tech/coner/trailer/presentation/view/json/internal/identifier/EventIdentifier.kt
+++
@@ -1,9 +0,0 @@
-package tech.coner.trailer.presentation.view.json.internal.identifier
-
-import tech.coner.trailer.Event
-import java.time.LocalDate
-import java.util.*
-
-class EventIdentifier(val key: UUID, val name: String, val date: LocalDate) {
-    constructor(event: Event) : this(key = event.id, name = event.name, date = event.date)
-}
