fb79b77cfd4a8fd0bf7bd6eb6032036d6a12b833
Carlton Whitehead <cebesius86@gmail.com>
2024-05-30 07:44:56 -0400

    SNAPSHOT: second draft presentation

---
+++ presentation/presentation-library/src/main/kotlin/tech/coner/trailer/presentation/library/model/LoadableModel.kt
@@ -0,0 +1,56 @@
+package tech.coner.trailer.presentation.library.model
+
+sealed class LoadableModel<ARGUMENT_MODEL, ITEM_MODEL> : Model {
+
+    /**
+     * Model represents presenter arguments
+     */
+    abstract val argument: ARGUMENT_MODEL
+
+    /**
+     * Initial model corresponding to the presenter having initial state,
+     * prior to starting to load anything, or if it was fully reset.
+     */
+    data class Empty<ARGUMENT_MODEL, ITEM_MODEL>(
+        override val argument: ARGUMENT_MODEL
+    ) : LoadableModel<ARGUMENT_MODEL, ITEM_MODEL>()
+
+    /**
+     * Model indicates the item is loading.
+     *
+     * @property partial Partial model representing incomplete loading
+     * state. For long or complex load operations, use to convey details
+     * of loading in progress. Adapters may optionally specify this value.
+     * It may be helpful to implement the partial model with a different
+     * type than the loaded item.
+     * @property prior the item that was previously loaded, if any.
+     */
+    data class Loading<ARGUMENT_MODEL, ITEM_MODEL>(
+        override val argument: ARGUMENT_MODEL,
+        val partial: ITEM_MODEL? = null,
+        val prior: ITEM_MODEL? = null
+    ) : LoadableModel<ARGUMENT_MODEL, ITEM_MODEL>()
+
+    /**
+     * Model indicates the item has loaded.
+     *
+     * @property item the item resulting from the load operation.
+     */
+    data class Loaded<ARGUMENT_MODEL, ITEM_MODEL>(
+        override val argument: ARGUMENT_MODEL,
+        val item: ITEM_MODEL
+    ) : LoadableModel<ARGUMENT_MODEL, ITEM_MODEL>()
+
+    /**
+     * Model indicates the load operation failed.
+     *
+     * @property priorItem represents the item that was loaded previously, prior to the load attempt which failed, if any
+     * @property cause the cause of the failure, if known
+     */
+    data class LoadFailed<ARGUMENT_MODEL, ITEM_MODEL>(
+        override val argument: ARGUMENT_MODEL,
+        val priorItem: ITEM_MODEL?,
+        val cause: Throwable?
+    ) : LoadableModel<ARGUMENT_MODEL, ITEM_MODEL>()
+
+}
